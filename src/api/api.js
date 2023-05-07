8f189b184e64e5be9341b6082303c7dd5814f863
Mikhail <mihail.zharavin65@gmail.com>
2023-05-07 22:45:19 +0300

    added fetch data from adge functions

--- src/api/api.js
+++ src/api/api.js
@@ -2,16 +2,12 @@ export const config = {
 	runtime: 'edge'
 };
 
-const wait = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
-
-async function getAlbum() {
-	const res = await fetch('https://jsonplaceholder.typicode.com/albums/1');
-	await wait(10000);
+async function getPosts() {
+	const res = await fetch('https://jsonplaceholder.typicode.com/posts');
 	return res.json();
 }
 
-export default function MyEdgeFunction(request) {
-	getAlbum().then((json) => console.log({ json }));
-
-	return new Response(`Hello, from ${request.url} I'm an Edge Function!`);
+export default function MyEdgeFunction(req) {
+	const data = getPosts();
+	return data;
 }
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,8 +2,17 @@
 	import MyEdgeFunction from '../api/api';
 	import '../app.css';
 	import { onMount } from 'svelte';
-
-	const a = MyEdgeFunction('https://jsonplaceholder.typicode.com/posts');
+	import PostItem from '../components/PostItem/PostItem.svelte';
+	import { readable } from 'svelte/store';
+	let posts = [];
+	onMount(async () => {
+		const response = await MyEdgeFunction('https://jsonplaceholder.typicode.com/posts');
+		posts = response;
+	});
 </script>
 
-<h1>Hallo</h1>
+{#each posts as item}
+	<li>{item.title}</li>
+{/each}
+
+<PostItem />
