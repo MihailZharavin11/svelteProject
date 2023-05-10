b790fb4fce89849b95a56b1a6fb256ff5493ebea
Mikhail <mihail.zharavin65@gmail.com>
2023-05-10 20:31:39 +0300

    Adding fetch data without realoading page, adding an effect to a button
    Adding data loading, without reloading the entire page. Adding an effect to the button when loading data

--- src/routes/posts/+page.svelte
+++ src/routes/posts/+page.svelte
@@ -7,7 +7,12 @@
 	let posts = [];
 	let dataLength = 10;
 	const limit = 100;
-	let loading = false;
+	let loading = true;
+
+	onMount(() => {
+		getDataFromEdgeFunction();
+	});
+
 	const getDataFromEdgeFunction = async () => {
 		loading = true;
 		const data = await MyEdgeFunction('https://jsonplaceholder.typicode.com/posts', dataLength);
@@ -15,32 +20,35 @@
 		loading = false;
 	};
 
-	onMount(() => {
-		getDataFromEdgeFunction();
-	});
+	const handleClickButton = () => {
+		if (dataLength < limit && posts[posts.length - 1].id < 100) {
+			dataLength += 10;
+			getDataFromEdgeFunction();
+		}
+	};
 </script>
 
 <div class="flex flex-col justify-center">
-	{#if loading}
-		<Loading />
-	{:else}
-		<div
-			class="justify-center px-2 xl:justify-start pt-11 mx-auto max-w-screen-xl flex flex-wrap align-middle gap-9"
-		>
-			{#each posts as item}
-				<PostItem id={item.id} title={item.title} body={item.body} processedAt={item.processedAt} />
-			{/each}
-		</div>
-		<button
-			on:click={() => {
-				if (dataLength < limit && posts[posts.length - 1].id < 100) {
-					dataLength += 10;
-					getDataFromEdgeFunction();
-				}
-			}}
-			class="self-center my-16 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 text-white font-bold py-2 px-4 rounded"
-		>
-			Load more...
-		</button>
-	{/if}
+	<div
+		class="justify-center px-2 xl:justify-start pt-11 mx-auto max-w-screen-xl flex flex-wrap align-middle gap-9"
+	>
+		{#each posts as item}
+			<PostItem id={item.id} title={item.title} body={item.body} processedAt={item.processedAt} />
+		{/each}
+	</div>
+	<button
+		disabled={loading}
+		on:click={handleClickButton}
+		class="{loading
+			? 'disabled'
+			: ''} self-center my-16 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 text-white font-bold py-2 px-4 rounded transition-all"
+	>
+		{loading ? 'Loading...' : 'Load more...'}
+	</button>
 </div>
+
+<style lang="scss">
+	.disabled {
+		@apply dark:bg-gray-400 transition-all;
+	}
+</style>
